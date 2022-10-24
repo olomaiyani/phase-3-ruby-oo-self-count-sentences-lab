73750d502199d0bbfd3089029a065d1bb016be1d
Nana <nanaolomaiyani@gmail.com>
2022-10-24 23:45:35 +0300

    completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
+    self.match(/.*[a-zA-Z][.]$/ )? true : false
     
   end
 
   def question?
+    self.match(/.*[a-zA-Z][?]$/ )? true : false
 
   end
 
   def exclamation?
+    self.match(/.*[a-zA-Z][!]$/ )? true : false
 
   end
 
   def count_sentences
+    self.split(/[.?!]+/).count
 
   end
 end
